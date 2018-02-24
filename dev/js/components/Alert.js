a92e95bfc3d4d53fffefab63a527a49abf9c789d
Theresa Lee <tsanglee@yipitdata.com>
2018-02-24 17:27:53 -0500

    Add alert component

---
+++ dev/js/components/Alert.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Alert = () => (
+    <div>
+      <div className="Rectangle">
+        <div className="BLOOD-CLOT-RISK">
+          Blood Clot Risk
+        </div>
+        <div className="blood-clot-info">
+          description
+        </div>
+      </div>
+    </div>
+);
+
+export default Alert;
