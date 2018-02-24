733f4982bab03ca4fbe2ab03f769abf0b35a4842
Theresa Lee <tsanglee@yipitdata.com>
2018-02-24 15:39:18 -0500

    Add constants

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,20 +1,30 @@
-const axios = require('axios');
+import * as types from '../constants.js'
+import axios from 'axios';
 
 export const selectUser = (user) => {
     console.log("You clicked on user: ", user.first);
-    // Make a request for a user with a given ID
-    axios.get('https://jsonplaceholder.typicode.com/posts/1')
-        .then(function (response) {
-            console.log('Sample Axios Call');
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log('Axios Call Gone Wrong');
-            console.log(error);
-        });
 
     return {
-        type: 'USER_SELECTED',
+        type: types.USER_SELECTED,
         payload: user
     }
 };
+
+export const getUserInfo = () => {
+      // Make a request for a user with a given ID
+    let ret = {};
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+        .then(function (response) {
+          ret = {
+            type: types.USERS_REQUEST,
+            payload: response
+          }
+        })
+        .catch(function (error) {
+            ret = {
+              types: types.USERS_FAILURE,
+              payload: {}
+
+            }
+        });
+}
---
+++ dev/js/constants.js
@@ -0,0 +1,6 @@
+export const USER_SELECTED = 'USER_SELECTED'
+export const USER_FAILURE = 'USER_FAILURE'
+
+export const USERS_REQUEST = 'USERS_REQUEST'
+export const USERS_SUCCESS = 'USERS_SUCCESS'
+export const USERS_FAILURE = 'USERS_FAILURE'
