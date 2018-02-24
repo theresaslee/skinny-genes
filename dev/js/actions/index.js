af4c97684c07260636c639d8bc419f8f676295ec
Ten <tenloh@gmail.com>
2018-02-24 13:45:20 -0500

    Installing Axios and Calling on Actions

--- dev/js/actions/index.js
+++ dev/js/actions/index.js
@@ -1,5 +1,18 @@
+const axios = require('axios');
+
 export const selectUser = (user) => {
     console.log("You clicked on user: ", user.first);
+    // Make a request for a user with a given ID
+    axios.get('https://jsonplaceholder.typicode.com/posts/1')
+        .then(function (response) {
+            console.log('Sample Axios Call');
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log('Axios Call Gone Wrong');
+            console.log(error);
+        });
+
     return {
         type: 'USER_SELECTED',
         payload: user
